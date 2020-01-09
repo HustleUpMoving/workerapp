4a0a50b53083791783d0780952796f6d56988bcc
Joe Stowers <greenchiletexan@gmail.com>
2020-01-08 20:07:23 -0600

    First commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jstowers/Documents/Coding/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jstowers/Documents/Coding/workerapp"
-export "FLUTTER_TARGET=/Users/jstowers/Documents/Coding/workerapp/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/jstowers/Documents/Coding/hustleup/workerapp"
+export "FLUTTER_TARGET=/Users/jstowers/Documents/Coding/hustleup/workerapp/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/jstowers/Documents/Coding/flutter/bin/cache/artifacts/engine/ios"
